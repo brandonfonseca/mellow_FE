2dec6f23728aeeaf352080bab231b8f6f7bdb973
Brandon Fonseca <brandonfonseca5@gmail.com>
2020-05-30 22:19:17 -0400

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Message from './CommonComponents/Message'
 
 const App = () => {
-  return <Message msg="Hello World!" />
+  return <Message msg="Hello World!"/>
 }
 
 export default App
